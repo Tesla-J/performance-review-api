846a6f23996f306b4a059d14db0af7f3fb7403b3
Rafael Marcos <rafix995@gmail.com>
2025-05-23 16:45:00 +0100

    New API endpoint for testing purposes

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,11 +3,17 @@ package dev.rmarcos
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.http.ContentType // or * as in the documentation
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/test") {
+            val text = "{\"Error\": \"This is working!\"}"
+            val type = ContentType.parse("application/json")
+            call.respondText(text, type)
         }
     }
 }
