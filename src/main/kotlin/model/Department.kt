8cc60d532a83f23950ea246b4ceb31c9c274f561
Rafael Marcos <rafix995@gmail.com>
2025-05-27 00:56:17 +0100

    Department Model created

---
+++ src/main/kotlin/model/Department.kt
@@ -0,0 +1,13 @@
+package dev.rmarcos.model
+
+import kotlinx.serialization.Serializable
+
+data class Department (
+    val id: Long,
+    val name: String,
+    val description: String,
+    // val manager: Profile
+    // manager attribute makes no sense here,
+    // I predict an infinite chain on serialization
+    // and a circular reference
+)
