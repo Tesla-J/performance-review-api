f881469cf47ddb07499b4c64161fc53a32885f8a
Rafael Marcos <rafix995@gmail.com>
2025-05-27 01:02:09 +0100

    Development Plan Model created

---
+++ src/main/kotlin/model/DevelopmentPlan.kt
@@ -0,0 +1,12 @@
+package dev.rmarcos.model
+
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+data class DevelopmentPlan (
+    val id: Long,
+    // val manager: Profile - does not make sense here
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val department: Department,
+)
