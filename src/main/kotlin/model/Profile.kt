f4b482a2050312df3af73e186ee99914ae72a080
Rafael Marcos <rafix995@gmail.com>
2025-05-27 01:38:04 +0100

    Removing underscore from Profile Model's attribute

--- src/main/kotlin/model/Profile.kt
+++ src/main/kotlin/model/Profile.kt
@@ -7,7 +7,7 @@ data class Profile (
     val id: Long,
     val name: String,
     val phone: String,
-    val alt_phone: String,
+    val altPhone: String,
     val role: String, // TODO find a better approach
     val department: Department
 )
